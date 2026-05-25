1ccfeaaff204470141e41ce70f2e8cbb65729512
Joan <Joanstiven883@gmail.com>
2026-05-25 08:14:14 -0500

    Implement hotel reservation validation rules
    Added rules and conditions for hotel reservation validation.

--- condicionales/reserva_hote.py
+++ condicionales/reserva_hote.py
@@ -1,3 +1,33 @@
+# ============================================
+# Ejercicio 8 — Reserva de hotel
+# ============================================
+
+# Un hotel necesita validar las reservas
+# realizadas por sus clientes.
+
+# Reglas:
+# - Solo las personas mayores de edad
+#   pueden reservar.
+#
+# - Debe haber habitaciones disponibles.
+
+# Condiciones de precio:
+# - Si la reserva es por más de 5 noches,
+#   aplicar un descuento del 15%.
+#
+# - Si es temporada alta,
+#   aumentar el valor total en un 25%.
+
+# El programa debe mostrar:
+
+# Si la reserva es válida:
+# - “Reserva realizada exitosamente.”
+# - Cantidad de noches.
+# - Valor total a pagar.
+
+# Si la reserva no es válida:
+# - “No es posible realizar la reserva.”
+
 edad = int(input("Ingrese la edad: "))
 habitaciones = input("¿Hay habitaciones disponibles? (si o no): ")
 noches = int(input("Ingrese la cantidad de noches: "))
