09a5cac582dca99aa3d43b7bfcc1405a6bfd74cf
Joan <Joanstiven883@gmail.com>
2026-05-25 08:14:48 -0500

    Implement admission process requirements in script
    Added requirements and rules for university admission process.

--- condicionales/proceso_admision.py
+++ condicionales/proceso_admision.py
@@ -1,3 +1,31 @@
+# ============================================
+# Ejercicio 9 — Proceso de admisión universitaria
+# ============================================
+
+# Una universidad desea automatizar
+# el proceso de admisión de estudiantes.
+
+# Requisitos de admisión:
+
+# El aspirante será admitido únicamente si:
+# - Tiene un puntaje igual o superior a 350.
+# - Aprobó la entrevista.
+
+# Reglas adicionales:
+# - Si el puntaje es mayor a 450,
+#   recibe beca parcial.
+#
+# - Si el puntaje es mayor a 500,
+#   recibe beca completa.
+#
+# - Si tiene sanciones disciplinarias,
+#   será rechazado automáticamente.
+
+# El programa debe mostrar:
+# - “Aspirante admitido.”
+# - “Aspirante rechazado.”
+# - Tipo de beca obtenida, si aplica.
+
 puntaje = int(input("Ingrese el puntaje: "))
 entrevista = input("¿Aprobó la entrevista? (si o no): ")
 sanciones = input("¿Tiene sanciones disciplinarias? (si o no): ")
