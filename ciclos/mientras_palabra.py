fbd37eef5b8031e387636e3a36431cc75ae4e13a
Joan <Joanstiven883@gmail.com>
2026-06-01 14:57:43 +0000

    last

--- ciclos/mientras_palabra.py
+++ ciclos/mientras_palabra.py
@@ -5,3 +5,18 @@ palabra = ""
 
 while palabra != "fin":
     palabra = input("Digite una palabra: ")
+
+
+
+
+#Realice un algoritmo que lea una palabra mientras no sea "fin" con un contador 
+
+contador = 0
+palabra = ""
+
+
+while palabra != "fin":
+    palabra = input("Digite una palabra: ")
+    contador = contador + 1
+
+print("La cantidad de palabras digitadas es: ", contador)
